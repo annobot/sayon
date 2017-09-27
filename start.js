31d80feeeb4ea83873281fdc01d670479f0e1e77
annobot <badalmishr7035@gmail.com>
2017-09-27 14:33:33 +0530

    Inting

---
+++ start.js
@@ -0,0 +1,34 @@
+var path =require('path');
+var express= require('express');
+var bodyParser = require('body-parser');
+var dir = require('node-dir');
+var walk    = require('walk');
+var files   = [];
+var server=express();
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+
+
+server.set('view engine','ejs');
+server.use('/',express.static(__dirname + '/assets'));
+server.get('/',function(req,res){
+  var walker  = walk.walk('./assets/slide/img', { followLinks: false });
+
+  walker.on('file', function(root, stat, next) {
+      // Add this file to the list of files
+
+      files.push(stat.name);
+      next();
+  });
+
+  walker.on('end', function() {
+      console.log(files);
+  });
+
+
+res.render('in',qs={files});
+console.log(qs);
+});
+
+server.listen(process.env.PORT || 3000);
+console.log('made it');
